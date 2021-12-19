165bc6cbc58a55db164168b6394a93b50451ad40
chukobyte <chukobyte@gmail.com>
2021-12-18 20:57:20 -0500

    Added functions to translate world into local.

--- include/re/scene/scene_node_utils.h
+++ include/re/scene/scene_node_utils.h
@@ -13,15 +13,36 @@ using CombineTransform2DFunction = std::function<Transform2DComponent(const Tran
 
 class SceneNodeUtils {
   public:
+    static Transform2DComponent TranslateEntityTransformIntoWorld(Entity entity) {
+        static ComponentManager* componentManager = ComponentManager::GetInstance();
+        Transform2DComponent entityTransform = GetEntityCombinedParentsTransforms(entity);
+        SceneComponent sceneComponent = componentManager->GetComponent<SceneComponent>(entity);
+        if (!sceneComponent.ignoreCamera) {
+            entityTransform = TranslateCamera2D(entityTransform);
+        }
+        return entityTransform;
+    }
+
+    static Transform2DComponent TranslateWorldTransformIntoLocal(Entity entity, const Transform2DComponent& worldTransform) {
+        static ComponentManager* componentManager = ComponentManager::GetInstance();
+        Transform2DComponent entityTransform = GetEntityDeCombinedParentsTransforms(entity);
+        SceneComponent sceneComponent = componentManager->GetComponent<SceneComponent>(entity);
+        if (!sceneComponent.ignoreCamera) {
+            entityTransform = DeTranslateCamera2D(entityTransform);
+        }
+        return entityTransform;
+    }
+
+  private:
     static Transform2DComponent GetEntityCombinedParentsTransforms(Entity entity) {
         static SceneManager* sceneManager = SceneManager::GetInstance();
         static ComponentManager* componentManager = ComponentManager::GetInstance();
-        static CombineTransform2DFunction combineTransformFunc = [](const Transform2DComponent& transformA, const Transform2DComponent& transformB) {
+        static CombineTransform2DFunction funcAddTransforms = [](const Transform2DComponent& transformA, const Transform2DComponent& transformB) {
             Transform2DComponent combinedTransform{};
             return Transform2DComponent{
                 transformA.position + transformB.position,
                 transformA.scale * transformB.scale,
-                transformA.rotation * transformB.rotation,
+                transformA.rotation + transformB.rotation,
                 transformA.zIndex + transformB.zIndex,
             };
         };
@@ -33,13 +54,42 @@ class SceneNodeUtils {
             SceneNode nodeParent = sceneManager->GetCurrentScene()->GetSceneNode(currentParent);
             if (componentManager->HasComponent<Transform2DComponent>(nodeParent.entity)) {
                 Transform2DComponent parentTransform = componentManager->GetComponent<Transform2DComponent>(nodeParent.entity);
-                combinedTransform = combineTransformFunc(combinedTransform, parentTransform);
+                combinedTransform = funcAddTransforms(combinedTransform, parentTransform);
             }
             currentParent = nodeParent.parent;
         }
         // Combine Entity and parent transforms
         Transform2DComponent entityTransform = componentManager->GetComponent<Transform2DComponent>(entity);
-        return combineTransformFunc(entityTransform, combinedTransform);
+        return funcAddTransforms(entityTransform, combinedTransform);
+    }
+
+    static Transform2DComponent GetEntityDeCombinedParentsTransforms(Entity entity) {
+        static SceneManager* sceneManager = SceneManager::GetInstance();
+        static ComponentManager* componentManager = ComponentManager::GetInstance();
+        static CombineTransform2DFunction funcSubTransforms = [](const Transform2DComponent& transformA, const Transform2DComponent& transformB) {
+            Transform2DComponent combinedTransform{};
+            return Transform2DComponent{
+                transformA.position - transformB.position,
+                transformA.scale / transformB.scale,
+                transformA.rotation - transformB.rotation,
+                transformA.zIndex - transformB.zIndex,
+                };
+        };
+        // Impl
+        SceneNode sceneNode = sceneManager->GetCurrentScene()->GetSceneNode(entity);
+        Transform2DComponent combinedTransform = Transform2DComponent{};
+        Entity currentParent = sceneNode.parent;
+        while (currentParent != NULL_ENTITY) {
+            SceneNode nodeParent = sceneManager->GetCurrentScene()->GetSceneNode(currentParent);
+            if (componentManager->HasComponent<Transform2DComponent>(nodeParent.entity)) {
+                Transform2DComponent parentTransform = componentManager->GetComponent<Transform2DComponent>(nodeParent.entity);
+                combinedTransform = funcSubTransforms(combinedTransform, parentTransform);
+            }
+            currentParent = nodeParent.parent;
+        }
+        // Combine Entity and parent transforms
+        Transform2DComponent entityTransform = componentManager->GetComponent<Transform2DComponent>(entity);
+        return funcSubTransforms(entityTransform, combinedTransform);
     }
 
     static Transform2DComponent TranslateCamera2D(const Transform2DComponent& transform2DComponent) {
@@ -51,13 +101,12 @@ class SceneNodeUtils {
         return translatedTransform;
     }
 
-    static Transform2DComponent TranslateEntityTransformIntoWorld(Entity entity) {
-        static ComponentManager* componentManager = ComponentManager::GetInstance();
-        Transform2DComponent entityTransform = GetEntityCombinedParentsTransforms(entity);
-        SceneComponent sceneComponent = componentManager->GetComponent<SceneComponent>(entity);
-        if (!sceneComponent.ignoreCamera) {
-            entityTransform = TranslateCamera2D(entityTransform);
-        }
-        return entityTransform;
+    static Transform2DComponent DeTranslateCamera2D(const Transform2DComponent& transform2DComponent) {
+        static CameraManager* cameraManager = CameraManager::GetInstance();
+        Transform2DComponent translatedTransform{};
+        Camera2D camera = cameraManager->GetCurrentCamera();
+        translatedTransform.position = (transform2DComponent.position + camera.viewport - camera.offset) / camera.zoom;
+        translatedTransform.scale = transform2DComponent.scale / camera.zoom;
+        return translatedTransform;
     }
 };
